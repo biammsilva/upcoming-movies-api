ae2e4d5fc7f32d645d6492762e27c5864d7c1bd0
Bia <beatriz@stormtech.com.br>
2019-08-29 00:13:17 -0300

    Changing the por to listen

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get('/', (req, res) => {
 var movie = require('./src/movie/routes.js');
 app.use('/movie', movie);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
